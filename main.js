6153ca2b7d0fd58adaf6bcd51ee3226351fb7a90
Mr. Gold <hochwilliam6@gmail.com>
2025-09-08 22:59:27 -0400

    dwasdwa

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var script = document.createElement('script')
 
 if (window.location.href.includes("https://quizlet.com")) {
     script.src = "https://cdn.jsdelivr.net/gh/educwiol/Hax@main/quizlet.js"
-    document.append(script)
+    document.body.append(script)
 }
